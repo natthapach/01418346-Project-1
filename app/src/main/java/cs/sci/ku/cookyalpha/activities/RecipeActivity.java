2ef4db6a9ba512fe0dc6695aa4901ee2bfbb3425
natthapach <natthapach.a@ku.th>
2017-11-08 22:14:31 +0700

    create RecipeActivity

---
+++ app/src/main/java/cs/sci/ku/cookyalpha/activities/RecipeActivity.java
@@ -0,0 +1,25 @@
+package cs.sci.ku.cookyalpha.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cs.sci.ku.cookyalpha.R;
+import cs.sci.ku.cookyalpha.fragments.PreviewRecipeFragment;
+
+public class RecipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe);
+
+        Intent intent = getIntent();
+        String recipeId = intent.getStringExtra("recipeId");
+        if (savedInstanceState == null && recipeId != null){
+            getSupportFragmentManager().beginTransaction()
+                                        .add(R.id.container_frame, PreviewRecipeFragment.newInstance(recipeId))
+                                        .commit();
+        }
+    }
+}
