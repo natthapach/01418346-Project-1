7fe1626a4388c1ceca5958b36855955634f344f5
natthapach <natthapach.a@ku.th>
2017-11-10 22:25:07 +0700

    use tab layout for recipe pager

--- app/src/main/java/cs/sci/ku/cookyalpha/activities/RecipeActivity.java
+++ app/src/main/java/cs/sci/ku/cookyalpha/activities/RecipeActivity.java
@@ -1,8 +1,13 @@
 package cs.sci.ku.cookyalpha.activities;
 
 import android.content.Intent;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TabWidget;
 
 import cs.sci.ku.cookyalpha.R;
 import cs.sci.ku.cookyalpha.fragments.IngredientListFragment;
@@ -11,17 +16,55 @@ import cs.sci.ku.cookyalpha.fragments.ProcedureListFragment;
 
 public class RecipeActivity extends AppCompatActivity {
 
+    private ViewPager viewPager;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
 
         Intent intent = getIntent();
-        String recipeId = intent.getStringExtra("recipeId");
-        if (savedInstanceState == null && recipeId != null){
-            getSupportFragmentManager().beginTransaction()
-                                        .add(R.id.container_frame, ProcedureListFragment.newInstance(recipeId))
-                                        .commit();
-        }
+        final String recipeId = intent.getStringExtra("recipeId");
+//        if (savedInstanceState == null && recipeId != null){
+//            getSupportFragmentManager().beginTransaction()
+//                                        .add(R.id.container_frame, ProcedureListFragment.newInstance(recipeId))
+//                                        .commit();
+//        }
+        viewPager = findViewById(R.id.viewPager);
+        viewPager.setAdapter(new FragmentStatePagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public CharSequence getPageTitle(int position) {
+                switch (position){
+                    case 0 :
+                        return "Preview";
+                    case 1 :
+                        return "Ingredients";
+                    case 2 :
+                        return "Procedure";
+                    default:
+                        return null;
+                }
+            }
+
+            @Override
+            public int getCount() {
+                return 3;
+            }
+
+            @Override
+            public Fragment getItem(int position) {
+                switch (position){
+                    case 0 :
+                        return PreviewRecipeFragment.newInstance(recipeId);
+                    case 1 :
+                        return IngredientListFragment.newInstance(recipeId);
+                    case 2 :
+                        return ProcedureListFragment.newInstance(recipeId);
+                    default:
+                        return null;
+                }
+            }
+        });
     }
 }
