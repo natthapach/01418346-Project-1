7f8200aa7a2f4c2c94da809fca87143115e8f54a
natthapach <natthapach.a@ku.th>
2017-11-14 22:51:41 +0700

    implement drawer menu

--- app/src/main/java/cs/sci/ku/cookyalpha/activities/MainActivity.java
+++ app/src/main/java/cs/sci/ku/cookyalpha/activities/MainActivity.java
@@ -1,7 +1,13 @@
 package cs.sci.ku.cookyalpha.activities;
 
+import android.content.res.Configuration;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import cs.sci.ku.cookyalpha.R;
 import cs.sci.ku.cookyalpha.fragments.GlobalRecipeListFragment;
@@ -9,6 +15,10 @@ import cs.sci.ku.cookyalpha.managers.FirebaseRecipeManager;
 import cs.sci.ku.cookyalpha.managers.RecipeManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawetLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private Toolbar toolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +37,32 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initInstance() {
+        toolBar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolBar);
+        drawetLayout = findViewById(R.id.drawerLayout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawetLayout, R.string.open_drawer, R.string.close_drawer);
+        drawetLayout.setDrawerListener(actionBarDrawerToggle);
 
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item))
+            return true;
+        return super.onOptionsItemSelected(item);
     }
 }
